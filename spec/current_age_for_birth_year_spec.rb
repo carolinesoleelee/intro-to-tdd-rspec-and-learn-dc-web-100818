9a06dfb7021278c052a2c5d76d505d59a217c418
Solee Lee <carolinesoleelee@gmail.com>
2018-09-18 18:36:34 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -4,7 +4,7 @@ describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
 
 def current_age_for_birth_year(year_born)
-  year_born - 2018
+  2018 - year_born
 end
 
   puts current_age_for_birth_year(1984)
